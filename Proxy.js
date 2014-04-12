bbd312aa042b3b8068f6a7df8b8904ecb059acba
ErosZy <395693101@qq.com>
2014-04-12 18:09:53 +0800

    inital

--- Proxy.js
+++ Proxy.js
@@ -16,8 +16,15 @@
     }
 
 }("Proxy",function(){
-    var splice = Array.prototype.splice;
+    var splice = Array.prototype.splice,
+        indexOf = Array.prototype.indexOf,
+        later = typeof process != "undefined" ? process.nextTick : setTimeout;
 
+    /**
+     * 构造函数
+     * @returns {Proxy}
+     * @constructor
+     */
     function Proxy(){
 
         if(!(this instanceof Proxy)){
@@ -28,27 +35,40 @@
         this._callbacks = [];
     }
 
+    /**
+     * all方法
+     * @param arr 事件名数组
+     * @param callback 所有事件完成后的回调函数
+     */
     Proxy.prototype.all = function(arr,callback){
         var self = this,
             len = 0,
             count = 0,
-            params = {};
+            params = {},
+            reg = /\s+/g,
+            _fnStr;
 
-        if(!self._is(arr,"Array")){
-            arr = [].push(arr);
-        }else{
-            arr = [].concat.apply([],arr);
-        }
+        arr = self._makeArray(arr);
 
         len = arr.length;
+
+        _fnStr = callback.replace(reg,'');
 
         for(var i = 0; i < len ; i++){
             var item = arr[i];
             self._binds[item] = self._binds[item] ? self._binds[item] : [];
-            self._binds[item].push(_bind);
+            self._binds[item].push({
+                callback : callback,
+                fnStr : _fnStr,
+                bind : _bind
+            });
         }
 
-        self._callbacks.push(_all);
+        self._callbacks.push({
+            fnStr : _fnStr,
+            binds : arr,
+            callback:_all
+        });
 
         function _bind(eventName,data){
             count++;
@@ -62,7 +82,7 @@
             var tmp = [];
 
             if(count < len){
-                return false;
+                return;
             }
 
             for(var i = 0; i < len ; i++){
@@ -71,11 +91,14 @@
             }
 
             callback.apply(self,tmp);
-
-            return true;
         }
     }
 
+    /**
+     * 触发对应的事件
+     * @param eventName 触发的事件名
+     * @param data 传入的参数
+     */
     Proxy.prototype.emit = function(eventName,data){
         var self = this,
             both = 2,
@@ -89,41 +112,133 @@
         while(both--){
             if(both){
                 for(var i = _proxy.length - 1; i >= 0; i--){
-                    _proxy[i].apply(self,[eventName,data]);
-                    splice.apply(_proxy,[i,1]);
+                    _proxy[i].bind.apply(self,[eventName,data]);
                 }
             }else{
                 for(var i = _all.length - 1 ; i >= 0 ; i--){
-                    var flag = _all[i].apply(self);
+                    _all[i].callback.apply(self);
+                }
+            }
+        }
+    }
 
-                    if(flag){
-                        splice.apply(_all,[i,1]);
+    /**
+     * 注册事件
+     * @param ev 事件名
+     * @param callback 回调函数
+     */
+    Proxy.prototype.addListener = function(eventName,callback){
+        var self = this,
+            len = arguments.length;
+
+        if(len < 2){
+            return;
+        }
+
+        self.all([eventName],callback);
+    }
+
+    /**
+     * 删除事件
+     * @param eventName 事件名
+     * @param callback 回调函数
+     */
+    Proxy.prototype.removeListener = function(eventName,callback){
+        var self = this,
+            len = arguments.length,
+            fnStr = callback.replace(/\s+/g,''),
+            _binds,fn,item,_all;
+
+        //删除所有回调事件
+        if(!len){
+            self._binds = {};
+            self._callbacks = [];
+        }else if(len < 2){
+            //删除对应事件的所有回调
+            _binds = self._binds[eventName];
+            for(var i = 0 ,len = _binds.length; i < len; i++){
+                fn = _binds.callback;
+                self.removeListener(eventName,fn);
+            }
+        }else{
+            //删除对应事件所有回调，并且找到对应的回调进行删除
+            _binds = self._binds[eventName];
+            _all = self._callbacks;
+
+            loop:for(var i = _binds.length - 1; i >= 0 ; i--){
+                item = _binds[i];
+                if(item.fnStr == fnStr){
+                    splice.call(_binds,[i,1]);
+                    for(var j = _all.length; j >= 0; j--){
+                        if(_all[j].fnStr == fnStr && self._inArray(eventName,_all[i].binds)){
+                            splice.call(_all,[i,1]);
+                            continue loop;
+                        }
                     }
                 }
             }
         }
     }
 
-    Proxy.prototype.addListener = function(ev,callback){
+    /**
+     * 删除对应事件的所有回调函数
+     * @param eventName
+     */
+    Proxy.prototype.removeAllListeners = function(eventName){
+        var self = this,
+            len = arguments.length;
 
+        if(!len){
+            self.removeListener();
+        }else{
+            self.removeListener(eventName);
+        }
     }
 
-    Proxy.prototype.removeListener = function(ev,callback){
+    /**
+     * 延迟触发
+     * @param eventName
+     * @param data
+     */
+    Proxy.prototype.emitLater = function(eventName,data){
+        var self = this;
 
+        later(function(){
+            self.emit(eventName,data);
+        },0);
     }
 
-    Proxy.prototype.removeAllListeners = function(){
+    /**
+     * 注册事件后立即触发
+     * @param arr
+     * @param callback
+     */
+    Proxy.prototype.immediate = function(arr){
+        var self = this,
+            len = arguments.length,
+            item,callback,data;
 
+        if(len < 3){
+            data = [];
+            callback = arguments[1];
+        }else{
+            data = self._makeArray(arguments[1]);
+            callback = arguments[2];
+        }
+
+        arr = self._makeArray(arr);
+
+        self.all(arr,callback);
+
+        for(var i = 0 , len = arr.length ; i < len ; i++){
+            item = arr[i];
+            self.emit(arr[i],data[i]);
+        }
     }
 
-    Proxy.prototype.emitLater = function(){
-
-    }
-
-    Proxy.prototype.immediate = function(){
-
-    }
-
+    /**
+     *
+     */
     Proxy.prototype.once = function(){
 
     }
@@ -158,6 +273,36 @@
 
     Proxy.prototype.doneLater = function(){
 
+    }
+
+    Proxy.prototype._inArray = function(item,arr){
+        var self = this,
+            index = -1;
+
+        if([].indexOf){
+            return indexOf.call(arr,item);
+        }else{
+            for(var i = 0, len = arr.length ; i < len ; i++){
+                if(arr[i] == item){
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        return index;
+    }
+
+    Proxy.prototype._makeArray = function(arr){
+        var self = this;
+
+        if(!self._is(arr,"Array")){
+            arr = [].push(arr);
+        }else{
+            arr = [].concat.apply([],arr);
+        }
+
+        return arr;
     }
 
     Proxy.prototype._is = function(obj,type){
